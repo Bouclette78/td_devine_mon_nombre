e74139de8945889e6da1a4520fad01b62639ea8f
Bouclette78 <elliotcurti@gmail.com>
2026-09-15 12:38:05 +0200

    Passage sous Cargo et ajout de la librairie rand

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::io;
+use rand::Rng;
 
 fn main() {
     println!("Devine mon nombre ! \n\nsaisissez votre proposition");
@@ -7,5 +8,8 @@ fn main() {
     let stdin = io::stdin();
     stdin.read_line(&mut buffer).unwrap();
 
-    println!("tu as choisi {}", buffer);
+    println!("tu as choisi {}", buffer.trim());
+
+    let rdm = rand::thread_rng().gen_range(1..101);
+    println!("nb aléatoire {}", rdm);
 }
