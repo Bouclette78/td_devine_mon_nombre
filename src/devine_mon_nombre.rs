cbdc845baa4fb2e9a308666bb7b02a9e0fe529c8
Bouclette78 <elliotcurti@gmail.com>
2026-09-15 11:47:33 +0200

    update

--- src/devine_mon_nombre.rs
+++ src/devine_mon_nombre.rs
@@ -1,4 +1,11 @@
+use std::io;
+
 fn main() {
-    println!("Hello World!");
+    println!("Devine mon nombre ! \n\nsaisissez votre proposition");
+    
+    let mut buffer = String::new();
+    let stdin = io::stdin();
+    stdin.read_line(&mut buffer).unwrap();
+
+    println!("tu as choisi {}", buffer);
 }
-/*test */
